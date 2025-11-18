f4bdd6f2ec563a730b5ebf63c6dea5b94e163433
Brufnie <adrianhesse123@gmail.com>
2025-11-18 12:46:45 +0100

    php hass

---
+++ berechnung.php
@@ -0,0 +1,14 @@
+<?php
+$produkte = [
+    ["name" => "self-php", "preis" => 25.40, "gewicht" => 800"],
+    ["name" => "php-ref", "preis" => 18.00, "gewicht" => "600"],
+    ["name" => "PHP-Kochbuch", "preis" => 39.00, "gewicht" => "1300"]
+];
+
+$mwst = 0.07;
+
+$Self = (int)$_POST['Self-PHP'];
+$PHP-Referenz = (int)$_POST['PHP-Referenz'];
+$PHP-Kochbuch = (int)$_POST['PHP-Kochbuch'];
+
+?php>
