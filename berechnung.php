ae57a8a25875914a726f1ffdbc4f2a7098123eb1
Brufnie <adrianhesse123@gmail.com>
2025-11-18 15:19:47 +0100

    anpassen formular

--- berechnung.php
+++ berechnung.php
@@ -1,14 +1,21 @@
 <?php
 $produkte = [
-    ["name" => "self-php", "preis" => 25.40, "gewicht" => 800"],
-    ["name" => "php-ref", "preis" => 18.00, "gewicht" => "600"],
-    ["name" => "PHP-Kochbuch", "preis" => 39.00, "gewicht" => "1300"]
+   'selfphp' => ["name" => "self-php", "preis" => 25.40, "gewicht" => 800],
+    'phpref' => ["name" => "php-ref", "preis" => 18.00, "gewicht" => 600],
+    'kochbuch' => ["name" => "PHP-Kochbuch", "preis" => 39.00, "gewicht" => 1300]
 ];
 
 $mwst = 0.07;
 
-$Self = (int)$_POST['Self-PHP'];
-$PHP-Referenz = (int)$_POST['PHP-Referenz'];
-$PHP-Kochbuch = (int)$_POST['PHP-Kochbuch'];
+$self = (int)$_POST['Self-PHP'];
+$ref = (int)$_POST['php-ref'];
+$koch = (int)$_POST['PHP-Kochbuch'];
 
-?php>
+if($self < 0 || $ref < 0 || $koch < 0){
+    echo "Ungültige Eingabe. Bitte geben Sie eine Zahl größer oder gleich 0 ein.";
+    exit;
+}
+
+
+
+?>
